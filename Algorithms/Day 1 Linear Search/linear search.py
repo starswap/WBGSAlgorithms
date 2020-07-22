d3856c1a69fa12116dd1bad08719e49ad1c32d81
Penguin5s <68483000+Penguin5s@users.noreply.github.com>
2020-07-22 17:59:05 +0100

    Create linear search.py

---
+++ Algorithms/Day 1 Linear Search/linear search.py
@@ -0,0 +1,14 @@
+#array = [1,2,3,4,5,6,7,8,9]
+
+target = input("what are you looking for?:   ")
+found  = False
+
+def lin_search(array,target):
+    for i in range(0, len (array)):
+        if array[i] == target:
+            print("found")
+            found = True
+            return found
+            
+if lin_search(array,target) != True:
+    print ("the item isn't in the list")
